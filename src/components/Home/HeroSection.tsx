e59253434c3258c3e847b43c7697aeecfebcf80f
Davin <davin-dj@hotmail.com>
2023-04-25 16:38:11 -0400

    Commit before attempting spotlight feature for hero section.

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -3,7 +3,10 @@ import { FC, ReactElement } from 'react';
 const HeroSection: FC = (): ReactElement => {
     return (
         <div id='hero-section'>
-            Hello there, I'm <span id='davin-jones'>&nbsp;Davin Jones</span>
+            <div id='hero-section-content'>
+                Hello there, I'm<span id='davin-jones'>&nbsp;Davin Jones</span>
+            </div>
+            <div id='spotlight-layer'></div>
         </div>
     )
 }
